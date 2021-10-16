4c6df8eb113c33caf49ef3f903e6a95219374a83
GitHubGM <glok0211@gmail.com>
2021-10-16 10:44:52 +0500

    UPD:antistring in int

--- exercise2.py
+++ exercise2.py
@@ -1,7 +1,11 @@
-n=int(input())
-sum=0
-i=1
-while i<=n:
-    sum=i+sum
-    i=i+1
-print("сумма",sum)
+try:
+    n=int(input("Ввудите значение n"))
+    sum=0
+    i=1
+    while i<=n:
+        sum=i+sum
+        i=i+1
+    print("сумма",sum)
+except:
+    print("Error")
+    exit()    
